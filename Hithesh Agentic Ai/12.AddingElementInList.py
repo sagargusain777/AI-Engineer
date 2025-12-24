5bc0fc92b26bfba719ccccaa8fb0fce1d0955fae
uwuxverticuse <sagargusain150@gmail.com>
2025-12-25 03:21:20 +0530

    Adding Element In List

---
+++ Hithesh Agentic Ai/12.AddingElementInList.py
@@ -0,0 +1,15 @@
+#Adding Element In List
+fruits = ["apple", "banana", "cherry"]
+print("Original Fruits List:", fruits)
+fruits.append("orange")  # Adding an element at the end
+print("Fruits List after append:", fruits)
+
+#Inserting an element at a specific position
+fruits.insert(1, "kiwi")  # Inserting 'kiwi' at index 1
+print("Fruits List after insert:", fruits)
+
+#Clear all elements from the list
+superheroes = ["Superman", "Batman", "Wonder"]
+print("superheroes List before clear:", superheroes)
+superheroes.clear()
+print("superheroes List after clear:", superheroes)
