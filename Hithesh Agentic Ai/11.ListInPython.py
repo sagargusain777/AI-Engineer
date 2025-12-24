9855387ffd7f9091cd0f693e19804a13f6dfd9a9
uwuxverticuse <sagargusain150@gmail.com>
2025-12-24 23:55:10 +0530

    List In  Python

---
+++ Hithesh Agentic Ai/11.ListInPython.py
@@ -0,0 +1,18 @@
+
+#Python List  are built-in data structures that can store multiple items in a single variable.
+#List can contain duplicate items 
+#List are mutable, meaning you can change their content without changing their identity.
+
+#Creating a List
+my_list = [1, 2, 3, 4, 5]
+print("Original List:", my_list)
+fruits = ["apple", "banana", "cherry"]
+print("Fruits List:", fruits)
+mixed_list = [1, "hello", 3.14, True]
+print("Mixed List:", mixed_list)
+#Accessing List Elements
+print("First Fruit in the Basket:", fruits[0])  # Output: apple
+print("Last Fruit in the Basket:", fruits[-1])  # Output: cherry
+print ("Slicing Fruits List:", fruits[1:3])  # Output: ['banana', 'cherry']
+
+
