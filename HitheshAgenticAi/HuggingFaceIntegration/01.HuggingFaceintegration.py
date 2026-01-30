2ed3584c0f20b6a66d4120a14fd745d7a0143911
Sagar Singh Gusain <sagargusain201012@gmail.com>
2026-01-30 22:35:05 +0530

    Hugging Face Integration

--- HitheshAgenticAi/HuggingFaceIntegration/01.HuggingFaceintegration.py
+++ HitheshAgenticAi/HuggingFaceIntegration/01.HuggingFaceintegration.py
@@ -2,7 +2,6 @@
 
 from transformers import pipeline
 
-pipe = pipeline("",model="gemma-3-270m-it")
 pipe = pipeline("text-generation", model="google/gemma-3-270m-it")
 messages = [
     {"role": "user", "content": "Who are you?"},
